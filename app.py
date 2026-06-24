2c1e9205f50b30e7ee4cbaa1120f676cf0030e4d
Security__Geek <amaankhan12e4@gmail.com>
2026-06-24 18:53:58 +0530

    Updated Instance Id

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv(
     "DATABASE_URL",
-    "sqlite:///network.db"
+    "sqlite:///network-traffic.db"
 )
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
